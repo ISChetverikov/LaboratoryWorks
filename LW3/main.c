aa449b3961614de046dbd5438bef17f1c6849f3c
Четвериков Илья <ischetverikov@gmail.com>
2017-11-04 16:15:44 +0300

    Comments in main

--- LW3/main.c
+++ LW3/main.c
@@ -7,15 +7,17 @@ int main() {
 	double X = 0, Y = 0;
 	double min = 0, avg = 0;
 	int returnCode = 0;
-	double * arr = (double *) 0;
+	double * arr = (double *) 0; // инициализация нулевым указателем
 
+	// Создание и вывод массива
 	K = createArray(&arr);
-
 	printArray(arr, K);
 
+	// Ввод ограничений для поиска минимума
 	printf_s("Please input range of elements for seeking minima:");
 	scanf_s("%lf %lf", &X, &Y);
 
+	// Вывод результата работы функции поиска минимума
 	if (minima(arr, K, X, Y, &min) == 0) {
 		printf_s(
 			"There is no elements greater or equal than %lf and less or equal %lf!\n",
@@ -27,6 +29,7 @@ int main() {
 		printf_s("Minima is %lf\n", min);
 	}
 
+	// Вывод результата работы функции поиска среднего
 	if (average(arr, K, &avg) == 0) {
 		printf_s("There is no elements after first maxima!\n");
 	}
@@ -34,6 +37,7 @@ int main() {
 		printf_s("Average value is %lf\n", avg);
 	}
 
+	// Очистка памяти, занятого массивом
 	freeArray(arr);
 	return;
 }
