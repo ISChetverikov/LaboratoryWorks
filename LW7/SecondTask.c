024bbf9b5d36e44a6848cf5003e124d5dd171cf9
Четвериков Илья <ischetverikov@gmail.com>
2018-02-28 20:52:32 +0300

    Created files for the second task

---
+++ LW7/SecondTask.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "SecondTask.h"
+
+#define SHORT_STRING_PROBABILITY 0.1
+#define LONG_WORD_PROBABILITY 0.1 
+
+
+int CreateSecondFile(char * filename, int stringsCount) {
+	return 0;
+}
---
+++ LW7/SecondTask.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int CreateSecondFile(char * filename, int stringsCount);
